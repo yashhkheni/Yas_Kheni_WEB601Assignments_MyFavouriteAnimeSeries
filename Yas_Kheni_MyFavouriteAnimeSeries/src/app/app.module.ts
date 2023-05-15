e1cf1ea72e926f35766c33742dceb55e730e6671
Yash kheni <113391607+yashhkheni@users.noreply.github.com>
2023-05-15 17:20:08 -0400

    Assignment-2
    Complete Assignment 2
    -Deleted Content-card
    -Created Content-List
    
    -Functionality works when we click on image...

--- Yas_Kheni_MyFavouriteAnimeSeries/src/app/app.module.ts
+++ Yas_Kheni_MyFavouriteAnimeSeries/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContentCardComponent } from './content-card/content-card.component';
+import { ContentListComponent } from './content-list/content-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentCardComponent
+    ContentListComponent
   ],
   imports: [
     BrowserModule
